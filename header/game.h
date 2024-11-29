7f22062e627c6899a6102ac549e3d2c2b2017533
Alexander Chabowski <alexader.chabowski@mbq-gmbh.de>
2024-11-29 14:44:43 +0100

    Init

---
+++ header/game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "raylib.h"
+
+#define SCREEN_WIDTH (1280)
+#define SCREEN_HEIGHT (736)
+#define WINDOW_TITLE "RlArc"
+
+
+void InitGame(int screenWidth, int screenHeight);
+void UpdateGame(void);
+void DrawGame(void);
+void UnloadGame(void);
+Color ColorFromHex(const char *hex);
+
+int **update(RenderTexture2D screen, int **cells, int **updated_cells, int size, bool with_progress);
+
+
+#endif // GAME_H
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include "game.h"
+
+int main(void) {
+    InitGame(1280, 720);
+
+    while (!WindowShouldClose()) {
+        UpdateGame();
+        DrawGame();
+    }
+
+    UnloadGame();
+
+    return 0;
+}
